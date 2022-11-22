c275ef06a8d9f9ab488dc571d8750453c3cca275
shizeying <w741069229@gmail.com>
2022-11-22 16:52:51 +0800

    作业

--- src/main/java/com/example/homework/HomeWorkShi1.java
+++ src/main/java/com/example/homework/HomeWorkShi1.java
@@ -1,4 +1,18 @@
 package com.example.homework;
 
 public class HomeWorkShi1 {
+		///数据1|2|3|4|5|6|7
+		//1.转换为一个数组：
+		//2.分别使用foreach;for;stream 转换为LIst集合
+		//3.分别使用foreach;for;stream 对里面的元素+1 转为一个新的list集合 小扩展 扩展点：stream中.map算子使用方法
+		//4.将2，和3的集合放到一个LIst<List>集合中
+		//5.将个LIst<List>的集合转换为List集合fori foreach stream
+		//6.将List集合转换为Map 其中key为里面的元素乘以10 value：为元素本身 （使用foreach、fori、stream（这里可以看下Collection.toMap）） 扩展提
+		
+		/**
+		 * https://colobu.com/2016/03/02/Java-Stream/
+		 *
+		 *
+		 * https://blog.csdn.net/u010046908/article/details/100580093
+		 */
 }
