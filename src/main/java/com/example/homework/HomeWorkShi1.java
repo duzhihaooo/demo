28e065192fdcab0f9ef3c688572489608c841ad2
shizeying <w741069229@gmail.com>
2022-12-27 19:46:02 +0800

    作业

--- src/main/java/com/example/homework/HomeWorkShi1.java
+++ src/main/java/com/example/homework/HomeWorkShi1.java
@@ -1,9 +1,12 @@
 package com.example.homework;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class HomeWorkShi1 {
 		///数据1|2|3|4|5|6|7
 		//1.转换为一个数组：
-		//2.分别使用foreach;for;stream 转换为LIst集合
+		//2.分别使用foreach;fori;stream 转换为LIst集合 https://vimsky.com/examples/usage/arrays-stream-method-in-java.html
 		//3.分别使用foreach;for;stream 对里面的元素+1 转为一个新的list集合 小扩展 扩展点：stream中.map算子使用方法
 		//4.将2，和3的集合放到一个LIst<List>集合中
 		//5.将个LIst<List>的集合转换为List集合fori foreach stream
@@ -16,5 +19,10 @@ public class HomeWorkShi1 {
 		 *
 		 * https://blog.csdn.net/u010046908/article/details/100580093
 		 * 前5个
+		 * https://vimsky.com/examples/usage/arrays-stream-method-in-java.html
+		 * https://segmentfault.com/a/1190000022887572 Lambda 表达式
+		 * https://juejin.cn/post/6980135669368619045 stream的基础
+		 * https://blog.csdn.net/u014042066/article/details/76372116
 		 */
+
 }
