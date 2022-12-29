4fc4451ab704b3694572549340ff7d93bcc226ad
duzhihaooo <duzhihaooo@163.com>
2022-12-29 23:21:55 +0800

    变更

--- src/main/java/com/example/homework/HomeWorkShi2.java
+++ src/main/java/com/example/homework/HomeWorkShi2.java
@@ -1,8 +1,6 @@
 package com.example.homework;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,6 +29,8 @@ public class HomeWorkShi2 {
 	 * https://juejin.cn/post/6980135669368619045 stream的基础
 	 * https://blog.csdn.net/u014042066/article/details/76372116
 	 */
+	
+	
 	public static void main(String[] args) {
 		//Q1:转换为一个数组：
 		String str1 = "1|2|3|4|5|6|7";
@@ -44,36 +44,76 @@ public class HomeWorkShi2 {
 		 * .。。。。
 		 * flatMap List
 		 */
-		List<Integer> listIntNew = new ArrayList<>();
-		List<Integer[]> list = new ArrayList<>();
 		//Q8:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，取出最大值
 		//[1,2,3,4,5,6,7] -> [1,2] [2,3] [3,4] [4,5] [5,6] [6,7] [7,8]
-		List<Integer> listInt1 = Arrays.stream(array).map(x->{
-			int[][] num =
-		}).collect(Collectors.toList());
-		System.out.println(listInt1);
+		List<List<Integer>> listNewListOneToTwo8 = Arrays.stream(array).map(x -> one2TwoNum(x)).collect(Collectors.toList());
+		System.out.println(listNewListOneToTwo8);	//[[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8]]
+		//todo:flatmap -> max 打平了之和我想都收集之后再进行比较怎么做
+		//todo:不确定这样是不是对的
+		//todo:Integer::compare  不用双引号该怎么写
+		int maxNum = listNewListOneToTwo8.stream().flatMap(x->x.stream()).max(Integer::compare).get();
+		System.out.println("newList中最大值："+maxNum);	//8
 		
+		//Q9:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，取出最小值
+		List<List<Integer>> listNewListOneToTwo9 = Arrays.stream(array).map(x->one2TwoNum(x)).collect(Collectors.toList());
+		System.out.println(listNewListOneToTwo9);	//[[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8]]
+		int minNum = listNewListOneToTwo9.stream().flatMap(x -> x.stream()).min(Integer::compare).get();
+		System.out.println("newList中最小值："+minNum);	//1
 		
+		//Q10:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，取出平均数
+		List<List<Integer>> listNewListOneToTwo10 = Arrays.stream(array).map(x -> one2TwoNum(x)).collect(Collectors.toList());
+		System.out.println(listNewListOneToTwo10);	//[[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8]]
+		Double averageNum = listNewListOneToTwo10.stream().flatMap(x -> x.stream()).collect(Collectors.averagingDouble(Integer::doubleValue));
+		System.out.println("newList中平均值："+averageNum);
 		
+		//Q11:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，取出平均数mapToInt().average()
+		//todo:需要讲解mapToInt
+		OptionalDouble averageMapToInt = listNewListOneToTwo10.stream().flatMap(x -> x.stream()).mapToInt(Integer::intValue).average();
+		System.out.println("mapToInt的平均值："+averageMapToInt.getAsDouble());
 		
+		//Q12:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，通过noneMatch、allMatch、anyMatch 进行对应的判断
+		//todo：该题是不是先要用flatmap打平后生成新的list 再通过对newList生成流再调用noneMatch、allMatch、anyMatch方法
+		final List<Integer> newList = listNewListOneToTwo10.stream().flatMap(x -> x.stream()).collect(Collectors.toList());
+		System.out.println("newList:"+newList);
+		boolean allmatch = newList.stream().allMatch(x->x.equals(1));
+		boolean anymatch = newList.stream().anyMatch(x->x.equals(1));
+		boolean nonematch = newList.stream().noneMatch(x->x.equals(11));
+		System.out.println("allmatch结果:"+allmatch);	//false
+		System.out.println("anymatch结果:"+anymatch);	//true
+		System.out.println("nonematch结果:"+nonematch);	//true
 		
+		//Q13:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，然后跳过前3个元素之后，获取最小值
+		System.out.println("newList:"+newList);	//newList:[1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8]
+		//todo:Integer::compare  不用双引号的写法
+		Integer minSkip3Num = newList.stream().skip(3).min(Integer::compare).get();
+		System.out.println("newList中跳过前3个元素之后，获取最小值："+minSkip3Num);
 		
+		//Q14:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，然后进行从大到小的排序，之后获取最大值
+		System.out.println("newList:"+newList);	//newList:[1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8]
+		//todo:sorted(Comparator.reverseOrder())  自然序逆序元素
+		int numMAxHighToLow = newList.stream().sorted(Comparator.reverseOrder()).max(Integer::compare).get();
+		System.out.println(numMAxHighToLow);
 		
+		//Q15:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，获取大于10的，并获取第一个元素
+		newList.add(12);
+		newList.add(13);
+		System.out.println("newList:"+newList);	//newList:[1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 12, 13]
+		List<Integer> numThan10First = newList.stream().filter(x -> x > 10).limit(1).collect(Collectors.toList());
+		System.out.println("newList中大于10的第一个元素："+numThan10First);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		//Q16:先执行第一1，然后转换为stream，通过.map操作将里面的元素+1,构成一个新的list集合，然后通过flatMap打平这个集合，获取大于5的，并获取任意一个元素
+		System.out.println("newList:"+newList);	//newList:[1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 12, 13]
+		//匹配任意元素(并行流)
+		int numThan5FindAny = newList.parallelStream().filter(x -> x > 5).findAny().get();
+		System.out.println("大于5的，并获取任意一个元素:"+numThan5FindAny);
 	}
+	
+	private static List<Integer> one2TwoNum(String x){
+		int i = Integer.parseInt(x);
+		int i1 = i+1;
+		return Arrays.asList(i, i1);
+	}
+	
 	
 	
 }
